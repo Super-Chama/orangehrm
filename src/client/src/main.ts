40493574a0cc5a0065d02b3fba51beaf1c402793
Chamara Abesinghe <chamara@orangehrmlive.com>
2023-02-15 11:41:38 +0530

    core, auth plugins added back

--- src/client/src/main.ts
+++ src/client/src/main.ts
@@ -1,14 +1,25 @@
 import {createApp} from 'vue';
 import {createPinia} from 'pinia';
 
-import App from './App.vue';
-import router from './router';
+import '@ohrm/oxd/fonts.css';
+import '@ohrm/oxd/icons.css';
+import '@ohrm/oxd/style.css';
+import './core/styles/global.scss';
+import './core/plugins/loader/loader.scss';
+import './core/plugins/toaster/toaster.scss';
 
-import './assets/main.css';
+import pages from './pages';
+// import router from './router';
+import components from './components';
 
-const app = createApp(App);
+// const app = createApp(App);
+const app = createApp({
+  name: 'App',
+  components: pages,
+});
 
 app.use(createPinia());
-app.use(router);
+app.use(components);
+// app.use(router);
 
 app.mount('#app');
--- src/client/vite.config.ts
+++ src/client/vite.config.ts
@@ -1,21 +1,49 @@
 import path from 'node:path';
 import {defineConfig} from 'vite';
 import vue from '@vitejs/plugin-vue';
+import {writeFile} from 'node:fs/promises';
 import {fileURLToPath, URL} from 'node:url';
 
 export default defineConfig({
-  plugins: [vue()],
+  base: './',
+  plugins: [
+    vue(),
+    {
+      name: 'DumpBuildTimestampPlugin',
+      async closeBundle() {
+        const now = Date.now().toString();
+        await writeFile(path.join(__dirname, '../../web/dist/build'), now);
+        console.info('Assets version: ', now);
+      },
+    },
+  ],
   build: {
-    manifest: false,
     emptyOutDir: true,
     outDir: path.resolve(__dirname, '../../web/dist'),
     rollupOptions: {
       input: path.resolve(__dirname, 'src/main.ts'),
+      output: {
+        inlineDynamicImports: true,
+        entryFileNames: 'js/app.js',
+        chunkFileNames: 'js/chunk-vendors.js',
+        assetFileNames: (assetFile) => {
+          return assetFile.name === 'main.css'
+            ? 'css/app.css'
+            : 'assets/[name].[ext]';
+        },
+      },
     },
   },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "@/core/styles";`,
+      },
     },
   },
 });
