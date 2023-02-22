fb69a3c67b2aeed01140ec671320ef22ea265ed9
Chamara Abesinghe <chamara@orangehrmlive.com>
2023-02-22 16:11:53 +0530

    PERMISSIONS

--- src/plugins/orangehrmAuthenticationPlugin/Controller/AppController.php
+++ src/plugins/orangehrmAuthenticationPlugin/Controller/AppController.php
@@ -19,18 +19,62 @@
 
 namespace OrangeHRM\Authentication\Controller;
 
+use OrangeHRM\Admin\Traits\Service\UserServiceTrait;
 use OrangeHRM\Core\Controller\AbstractVueController;
+use OrangeHRM\Core\Helper\VueControllerHelper;
+use OrangeHRM\Core\Traits\UserRoleManagerTrait;
 use OrangeHRM\Core\Vue\Component;
+use OrangeHRM\Core\Vue\Prop;
+use OrangeHRM\Entity\Employee;
+use OrangeHRM\Entity\WorkflowStateMachine;
 use OrangeHRM\Framework\Http\Request;
+use OrangeHRM\Pim\Traits\Service\EmployeeServiceTrait;
 
 class AppController extends AbstractVueController
 {
+    use UserServiceTrait;
+    use UserRoleManagerTrait;
+    use EmployeeServiceTrait;
+
     /**
      * @inheritDoc
      */
     public function preRender(Request $request): void
     {
         $component = new Component('app');
+        $component->addProp(
+            new Prop(
+                'unselectable-emp-numbers',
+                Prop::TYPE_ARRAY,
+                $this->getEmployeeService()->getUndeletableEmpNumbers()
+            )
+        );
+        $component->addProp(new Prop('unselectable-ids', Prop::TYPE_ARRAY, $this->getUserService()->getUndeletableUserIds()));
         $this->setComponent($component);
+
+        $allowedToDeleteActive = $this->getUserRoleManager()->isActionAllowed(
+            WorkflowStateMachine::FLOW_EMPLOYEE,
+            Employee::STATE_ACTIVE,
+            WorkflowStateMachine::EMPLOYEE_ACTION_DELETE_ACTIVE
+        );
+        $allowedToDeleteTerminated = $this->getUserRoleManager()->isActionAllowed(
+            WorkflowStateMachine::FLOW_EMPLOYEE,
+            Employee::STATE_TERMINATED,
+            WorkflowStateMachine::EMPLOYEE_ACTION_DELETE_TERMINATED
+        );
+        $permissionsArray['employee_list'] = [
+            'canRead' => true,
+            'canCreate' => $this->getUserRoleManager()->isActionAllowed(
+                WorkflowStateMachine::FLOW_EMPLOYEE,
+                Employee::STATE_NOT_EXIST,
+                WorkflowStateMachine::EMPLOYEE_ACTION_ADD
+            ),
+            'canUpdate' => true,
+            'canDelete' => $allowedToDeleteActive || $allowedToDeleteTerminated,
+        ];
+        $this->getContext()->set(
+            VueControllerHelper::PERMISSIONS,
+            $permissionsArray
+        );
     }
 }
